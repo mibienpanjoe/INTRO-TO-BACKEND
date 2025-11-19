d1af3310d0845f9e5e20c3eec06a997c2f4de205
mibienpanjoe <epixprint00@gmail.com>
2025-11-19 22:57:27 +0000

    created src folder , app file and database file

---
+++ backend/src/config/constants.js
@@ -0,0 +1,9 @@
+export const DB_NAME = "intro-to-backend";
+
+const connectDB = async ()=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
---
+++ backend/src/config/database.js
@@ -0,0 +1,1 @@
+import moogoose from "mongoose";
